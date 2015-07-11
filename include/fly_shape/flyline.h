5f592978f25495b160dcc624877d8b3592730075
Xuancong Lee <congleetea@gmail.com>
2015-07-11 10:57:32 +0800

    correct wrong name shaple to shape

---
+++ include/fly_shape/flyline.h
@@ -0,0 +1,48 @@
+#ifndef FLYLINE_H
+#define FLYLINE_H
+
+#include "ros/ros.h"
+#include "ros/time.h"
+#include <geometry_msgs/PoseStamped.h>
+#include "tf/transform_broadcaster.h"
+
+#include <math.h>
+#include <eigen3/Eigen/Eigen>
+
+using namespace Eigen;
+
+class FlyLine
+{
+public:
+    FlyLine();
+    void localCallBack(const geometry_msgs::PoseStampedConstPtr& msg);
+
+private:
+    Vector3d beginpoint_;
+    Vector3d endpoint_;
+
+    double   AllowError;
+    double   line_t;
+    double   land_t;
+    double   allline_t;  // the time from the begin point to end point of line
+    double   allland_t;
+    ros::Time       t_now;
+    ros::Time       t_prev;
+    ros::NodeHandle l_nh_;
+    ros::Subscriber local_sub_;
+    ros::Publisher  pos_sp_pub_;
+    ros::Publisher  att_sp_pub_;
+
+    geometry_msgs::PoseStamped pose_pub_;
+
+    bool    in_takeoff;
+    bool    in_line;
+    bool    in_land;
+
+    void takeoff(Vector3d bp);
+    void land(Vector3d ep, double t);
+    void line(Vector3d bp, double t, Vector3d ep);
+    void publish();
+};
+
+#endif // FLYLINE_H
